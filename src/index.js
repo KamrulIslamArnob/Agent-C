0ed5c0ee7297717a77e7d92ee38dcda730f2c1d5
Kaium Al Limon <limon.3cs@gmail.com>
2025-03-14 01:40:28 +0600

    Set up Express server with routes and database connection

---
+++ src/index.js
@@ -0,0 +1,36 @@
+// imports and configs
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const dotenv = require('dotenv');
+dotenv.config();
+const { connectDB } = require('./configs/database.config');
+// test route
+const  testRoutes  = require('./routes/test.routes');
+// question route
+const questionRoutes = require('./routes/question.routes');
+
+
+// application setup
+const app = express();
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// database connection
+connectDB();
+
+
+// routes
+app.use('/api/test', testRoutes);
+app.use('/api/question', questionRoutes);
+
+
+
+// server setup
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port https://localhost:${PORT}`);
+});
+
