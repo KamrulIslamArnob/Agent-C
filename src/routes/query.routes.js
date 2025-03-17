25587d911482072192eff3bb332ee2b6f54103be
Kaium Al Limon <limon.3cs@gmail.com>
2025-03-17 16:39:02 +0600

    route protection add through auth middleware

--- src/routes/query.routes.js
+++ src/routes/query.routes.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const queryController = require('../controllers/query.controller');
+const protect = require('../middlewares/authorization.middleware');
 
-
-// routes
-router.post('/ask',queryController.askQuestion);
+// Routes
+router.post('/ask', protect, queryController.askQuestion);
 
 module.exports = router;
